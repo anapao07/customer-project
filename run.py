a5baf36977258f6d5c4c267e3e6f7d4db0aa2666
Ana Paola <annapao.arcos@gmail.com>
2019-07-22 21:40:16 -0500

    Create Graph Customer

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from flask import redirect, url_for
 import sqlite3 
 import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
-
+import numpy as np	
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -67,6 +67,7 @@ def insert_product_customer(id,lista):
 @app.route("/assignation")
 def get_data_cus():
     try:
+        graphical_customerspro()
         conn = connection_db()
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
@@ -332,15 +333,25 @@ def update_customer(id):
 
 def graphical_product():
     try:
+        dateTimeObj = datetime.now()
         conn = connection_db()
         conn.row_factory = lambda cursor, row: row[0]
+        c = conn.cursor()
         cantidad = c.execute('select cantidad value from producto').fetchall()
+        print(cantidad)
         c = c.execute("SELECT nombre FROM producto")
         lab =c.fetchall()
+        print(lab)
         sizes = cantidad
         fig1, ax1 = plt.subplots()
-        ax1.pie(sizes,labels=lab,autopct='%1.0f%%',shadow=True, startangle=100)
-        plt.savefig("static/image/productsc.png")
+        ax1.pie(sizes,labels=lab,autopct='%1.0f%%',
+                shadow=True, startangle=100)
+        ax1.axis('equal')  
+       
+        timeStr = dateTimeObj.strftime("%H:%M:%S")
+        print('Current Timestamp : '+ timeStr)
+        plt.savefig("static/image/products"+ timeStr)
+        plt.show()
     except Exception as e:
         print(e)
     finally:
@@ -381,6 +392,85 @@ def graphical_productsb():
         print(e)
     finally:
         conn.close()
+
+
+def graphical_customerspro():
+    try:
+        conn = connection_db()
+        c = conn.cursor()
+        resutados_cliente_producto = c.execute("""SELECT c.nombre as cliente,
+                                  p.nombre as producto, p.cantidad 
+                            FROM custom_prod AS cp LEFT JOIN  customer as c on 
+                             c.id = cp.customer_id 
+                            LEFT JOIN producto as p on cp.product_id = p.id""").fetchall()
+        productos = c.execute("SELECT nombre FROM producto").fetchall()
+        clientes = c.execute("SELECT nombre FROM customer").fetchall()
+        # Generador que convierte las tuplas en lista 
+        lista_todos_productos = [value[0] for value in productos]
+        # Inicalizar el diccionario con los clientes como llaves - None significa Nulo
+        cliente_producto = {}
+        results = {}
+        for cliente in clientes:
+            cliente_producto[cliente[0]] = None
+            results[cliente[0]] = None 
+            # A todos los clientes asignar todos los productos con cantidad cero (inicialización)
+        for cliente in cliente_producto.keys():
+            cliente_producto[cliente] = dict.fromkeys(lista_todos_productos, 0)
+            """ 
+        La tupla clientes, productos, cantidad sigue los indices:
+            - tupla[0] es Nombre del cliente
+            - tupla[1] es Nombre del producto
+            - tupla[2] es la Cantidad
+        """
+        for value in resutados_cliente_producto:
+            cliente_producto[value[0]][value[1]] = value[2]
+            
+        values_clientes = {}
+            
+        for value,key in zip(cliente_producto.values(), cliente_producto.keys()):
+            values_clientes[key] = list(value.values())
+            
+        category_names = productos
+        results = values_clientes
+        survey(results, category_names)
+
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()   
+
+
+def survey(results, category_names):
+   
+    labels = list(results.keys())
+    data = np.array(list(results.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = plt.get_cmap('RdYlGn')(
+    np.linspace(0.15, 0.85, data.shape[1]))
+
+    fig, ax = plt.subplots(figsize=(15, 8))
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        ax.barh(labels, widths, left=starts, height=0.6,
+                label=colname, color=color)
+        xcenters = starts + widths / 2
+
+        r, g, b, _ = color
+        text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        for y, (x, c) in enumerate(zip(xcenters, widths)):
+            ax.text(x, y, str(int(c)), ha='center', va='center',
+                    color=text_color)
+    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1),
+              loc='lower left', fontsize='small')
+    plt.savefig("static/image/productslista.png")
+
+    return fig, ax
+    
 
 
 if __name__ == "__main__":
